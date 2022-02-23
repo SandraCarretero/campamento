86e65bd03613713e21b2bb74b3940a24c6d6c340
SandraCarretero <sandracarretero24@gmail.com>
2022-02-23 18:33:55 +0100

    js aparecen clases de los lados al hacer scroll

--- js/funciones.js
+++ js/funciones.js
@@ -9,13 +9,18 @@ $(function(){
         else{
             $(".nav").removeClass("change");
         }
-        // scroll clases
-
-        scrollAnimation( "#clase1", "aparece-izq" );
-        scrollAnimation( "#clase2", "aparece-dcha" );
-
-    
+        
+        
     });
+    
+    $(window).scroll(function(){
+        // scroll clases
+    
+        scrollAnimation( "#animacion", "aparece-izq" );
+        scrollAnimation( "#animacion2", "aparece-dcha" );
+        
+    });
+    
 
 
 
@@ -37,15 +42,22 @@ $(function(){
 
 
 function scrollAnimation( id, clase ) {
+    // indica cantidad de pixeles que hacemos scroll
     var scroll = $(window).scrollTop();
-
+      
+    // cantidad de pixeles de distancia del objeto animado con respecto del top del documento
+    // .top para obtener solo esa propiedad
     var distancia_elemento = $(id).offset().top;
 
+    // para sumar o restar la altura de las cajas
     var ventana = $(window).height();
+    console.log("altuta ventana:"+ventana);
 
+    // si el scroll es mayor que la distancia del elemento, añadimos clase
     if(scroll+ventana/2 > distancia_elemento ){
         $(id).addClass(clase);
     }
+    // para quitarlo al subir 
     else{
         $(id).removeClass(clase);
     }
